a9f0d100c1a115dc130a6adbcc7b4bd168f0cd68
Naseem Ahmad <109871786+naseem12007571@users.noreply.github.com>
2023-03-27 23:12:49 +0530

    Day 1

---
+++ check whether a number is positive or negative.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    int num;
+    cin>>num;
+    if(num>0){
+        cout<<" Number is positive"<<endl;
+    }else if(num<0){
+        cout<<" Number is nigative"<<endl;
+    }else{
+        cout<<"zero";
+    }
+}
