1639e0867473bb1d6e4d1c5f05bbf203e484f6e5
Naseem Ahmad <109871786+naseem12007571@users.noreply.github.com>
2023-03-27 23:28:43 +0530

    Day 2

---
+++ Number even or odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int num;
+    cout<<"Enter the num add and even";
+    cin>>num;
+    if(num % 2==0){
+        cout<<"Number is even ";
+    }else{
+        cout<<"Number is 0dd";
+    }
+}
+
