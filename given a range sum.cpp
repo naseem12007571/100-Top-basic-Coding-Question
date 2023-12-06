01508b06dc0fe354dbdd592849a07110c1ad09de
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-12-07 00:24:02 +0530

    Create given a range sum.cpp

---
+++ given a range sum.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+   int n1= 5;
+   int n2= 10;
+   int sum=0;
+    
+    for(int i=n1;i<=n2;i++){
+        sum=sum+i;
+    }
+    cout<<sum;
+   
+    
+}
+
+range diya h 5 se 10 ke bich ka sum  45 ayega 
